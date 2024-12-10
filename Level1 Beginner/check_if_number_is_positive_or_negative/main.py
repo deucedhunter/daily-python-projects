e48f231edf66bf6a2aeca745af4cdde3f3f7f94f
deucedhunter <deucedhunter@gmail.com>
2024-12-10 18:21:07 +0100

    Level 1: Beginner "Check if Number is Positive or Negative"

---
+++ Level1 Beginner/check_if_number_is_positive_or_negative/main.py
@@ -0,0 +1,29 @@
+"""Project Description
+
+Create a simple program that checks if a given number is positive, negative, or zero.
+Expected Output
+Any time the program is executed, it prints out different messages based on the number sign:
+"""
+
+
+# Solution 1
+# casting number to integer and treat as whole
+# try:
+#     number = int(input("Enter a number: "))
+#     if number > 0:
+#         print("The number is positive.")
+#     elif number < 0:
+#         print("The number is negative.")
+#     else:
+#         print("The number is zero.")
+# except ValueError:
+#     print("Program works only on numbers.")
+
+# Solution 2
+text = input("Enter a number: ")
+if text[0] == '-':
+    print("The number is negative.")
+elif text != "0":
+    print("The number is positive.")
+else:
+    print("The number is zero.")
