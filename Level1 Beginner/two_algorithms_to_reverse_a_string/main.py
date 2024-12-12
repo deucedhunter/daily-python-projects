13bf249a5a75811b6cd934d4a3766325175a938c
deucedhunter <deucedhunter@gmail.com>
2024-12-12 14:51:35 +0100

    Level 1: Beginner "Two Algorithms to Reverse a String"

---
+++ Level1 Beginner/two_algorithms_to_reverse_a_string/main.py
@@ -0,0 +1,14 @@
+"""
+https://dailypythonprojects.substack.com/p/two-algorithms-to-reverse-a-string
+
+Project Description
+Your task for today is to write some code that reverses any string. There are two fundamentally different ways to accomplish this, one using a for-loop and another using string slicing. If you can, write both solutions.
+"""
+text = "Python is fun!"
+
+# Solution 1 - string slicing
+# print(text[::-1])
+
+# Solution 2
+# for letter in reversed(text):
+#     print(letter, end="")
